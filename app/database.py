3a10cf0c0efae5a706b846e7c639165f2c6c9683
Aristide Minganodji <aristideminganodji@gmail.com>
2026-03-12 19:58:06 +0100

    Arret du suvie du fichier .env

--- app/database.py
+++ app/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL="postgresql://tfastapi_user:password123@localhost/tfastapi_classic"
+load_dotenv()
+DATABASE_URL=os.getenv("DATABASE_URL")
 
 engine=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
